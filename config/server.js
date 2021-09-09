a3aa9eb0f0784ce4b1a0f7c92958ed57e9b2e1e4
Piero A. Pretto <pretto417@gmail.com>
2021-09-09 12:13:02 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '57af352bcd5efd8c46664decfe52d028'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e4f1848b-a1b9-4c0d-af10-36f80efbbb6a'
+};
